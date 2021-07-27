d6b945a15b859d1ef2bd001058bb610c03fc4dbf
gpsiegel <gpsiegel@hotmail.com>
2021-07-26 23:06:28 -0500

    first

---
+++ main.tf
@@ -0,0 +1,36 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
+resource "aws_instance" "jenkins_instance" {
+    #Always using the most recent version of Ubuntu 20.04
+    ami = "${data.aws_ami.ubuntu.id}"
+
+    instance_type = "t2.small"
+
+
+    subnet_id = "${aws_subnet.jenkins_subnet.id}"
+    security_group_ids = ["${aws_security_group.jenkins_instance.id}"]
+    associate_public_ip_address = true
+
+    # We're assuming there's a key with this name already
+    key_name = "build"
+
+    #Ansible Command to configure Jenkins on this instance
+    provisioner "local-exec" {
+        command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ./build.pem -i '${aws_instance.jenkins_instance.public_ip},' playbook.yml"
+    }
+}
---
+++ vpc.tf
@@ -0,0 +1,69 @@
+/*
+data "external" "myipaddr" {
+  program = ["bash", "-c", "curl -s 'https://ipinfo.io/json'"]
+}
+*/
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" jenkins_vpc {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "jeknins_gw" {
+  vpc_id = aws_vpc.jenkins_vpc
+}
+
+resource "aws_subnet" "jenkins_subnet" {
+  vpc_id     = aws_vpc.jenkins_vpc.id
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "Jenkins SG"
+  description = "Allow Ports to Configure Jenkins Instance"
+  vpc_id      = aws_vpc.jenkins_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.jenkins_vpc.cidr_block]
+  }
+
+    ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.jenkins_vpc.cidr_block]
+  }
+
+    ingress {
+    description      = "Jenkins Ports from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.jenkins_vpc.cidr_block]
+  }
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.jenkins_vpc.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
